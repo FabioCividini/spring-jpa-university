4a2620dde33d2a7c8ec737821f45c0e33e04122d
FabioCividini <82501787+FabioCividini@users.noreply.github.com>
2022-01-07 17:47:28 +0100

    Esercizio finito

--- spring-jpa-university/src/main/java/org/generation/italy/controller/DepartmentController.java
+++ spring-jpa-university/src/main/java/org/generation/italy/controller/DepartmentController.java
@@ -1,23 +1,33 @@
 package org.generation.italy.controller;
 
+import org.generation.italy.model.Department;
 import org.generation.italy.repository.DepartmentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/")
 public class DepartmentController {
-	
-	@Autowired
-	private DepartmentRepository repository;
 
+	@Autowired
+	private DepartmentRepository repo;
+	
+	
 	@GetMapping
-	public String departments(Model model) {
-		model.addAttribute("departments", repository.findAll(Sort.by("name")));
+	public String departmentList(Model model) {
+		model.addAttribute("list", repo.findAll(Sort.by("name")));
 		return "departments";
+	}
+	
+	@GetMapping("/department/{id}")
+	public String departmentDetail(Model model, @PathVariable Integer id) {
+		Department dep = repo.getById(id);
+		model.addAttribute("department", dep);
+		return "details";
 	}
 }
--- spring-jpa-university/src/main/java/org/generation/italy/model/Degree.java
+++ spring-jpa-university/src/main/java/org/generation/italy/model/Degree.java
@@ -1,100 +1,72 @@
 package org.generation.italy.model;
 
-import java.util.List;
-
-import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
-import javax.persistence.OneToMany;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 @Entity
-@Table(name="departments")
-public class Department {
+@Table(name="degrees")
+public class Degree {
 
 	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@GeneratedValue(strategy= GenerationType.IDENTITY)
 	private Integer id;
 	
 	private String name;
+	private String level;
 	private String address;
-	private String phone;
 	private String email;
 	private String website;
 	
-	@Column(name="head_of_department")
-	private String head;
-
-	@OneToMany
+	@ManyToOne
 	@JoinColumn(name="department_id")
-	private List<Degree> degree;
+	private Department department;
 	
 	public Integer getId() {
 		return id;
 	}
-
 	public void setId(Integer id) {
 		this.id = id;
 	}
-
 	public String getName() {
 		return name;
 	}
-
 	public void setName(String name) {
 		this.name = name;
 	}
-
+	public String getLevel() {
+		return level;
+	}
+	public void setLevel(String level) {
+		this.level = level;
+	}
 	public String getAddress() {
 		return address;
 	}
-
 	public void setAddress(String address) {
 		this.address = address;
 	}
-
-	public String getPhone() {
-		return phone;
-	}
-
-	public void setPhone(String phone) {
-		this.phone = phone;
-	}
-
 	public String getEmail() {
 		return email;
 	}
-
 	public void setEmail(String email) {
 		this.email = email;
 	}
-
 	public String getWebsite() {
 		return website;
 	}
-
 	public void setWebsite(String website) {
 		this.website = website;
 	}
-
-	public String getHead() {
-		return head;
+	public Department getDepartments() {
+		return department;
 	}
-
-	public void setHead(String headOfDepartment) {
-		this.head = headOfDepartment;
+	public void setDepartments(Department departments) {
+		this.department = departments;
 	}
-
-	public List<Degree> getDegrees() {
-		return degree;
-	}
-
-	public void setDegree(List<Degree> degree) {
-		this.degree = degree;
-	}
-	
 	
 }
